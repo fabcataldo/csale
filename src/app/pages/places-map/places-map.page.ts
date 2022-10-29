8d650c737275e49b76c0dcbf34f99fa43425c9b8
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-10-28 23:21:32 -0300

    Initial logic for obtain geocoder input, and to marker it

--- src/app/pages/places-map/places-map.page.ts
+++ src/app/pages/places-map/places-map.page.ts
@@ -1,9 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-
-import { environment } from 'src/environments/environment';
-import * as Mapboxgl from 'mapbox-gl';
-import * as MapboxGeocoder from 'mapbox-gl-geocoder';
-import * as GeoJSON from 'geojson';
+import { MapCustomService } from 'src/app/services/map-custom/map-custom.service';
 
 @Component({
   selector: 'app-csale-places-map',
@@ -12,56 +8,24 @@ import * as GeoJSON from 'geojson';
 })
 export class PlacesMapPage implements OnInit {
   @ViewChild('asGeoCoder', { static: true }) asGeocoder: ElementRef;
-  map: Mapboxgl.Map;
-  cbAddress;
+
+  geocoderAddressResult;
   
-  constructor(private renderer2: Renderer2) { }
+  constructor(private renderer2: Renderer2, private mapCustomService: MapCustomService) { }
 
   ngOnInit() {
-
-    (Mapboxgl as any).accessToken = environment.mapboxKey;
-    this.map = new Mapboxgl.Map({
-      container: 'map', // container ID
-      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-64.1915012, -31.4265492], // starting position [lNG, lat]
-      zoom: 15.25 // starting zoom,
-    });
-
-    // Add zoom and rotation controls to the map.
-    this.map.addControl(new Mapboxgl.NavigationControl());
-    this.map.addControl(new Mapboxgl.GeolocateControl({
-      positionOptions: {
-          enableHighAccuracy: true
-      },
-      trackUserLocation: true
-  }));
-
-    const geocoder = new MapboxGeocoder({
-      accessToken: environment.mapboxKey,
-      mapboxgl: Mapboxgl,
-      placeholder: '¿A donde queres ir hoy?'
-    });
-    
-    geocoder.on('result', ($event) => {
-      const {result} = $event;
-      // geocoder.clear();
-      console.log('result: ', result);
-      this.cbAddress.emit(result);
+    this.mapCustomService.geocoderAddressChange.subscribe(response=>{
+      this.geocoderAddressResult = response;
+      this.mapCustomService.addMarker(this.geocoderAddressResult.geometry.coordinates);
     })
-    
-    console.log('Tgeocoder');
-    console.log(geocoder);
-    this.renderer2.appendChild(this.asGeocoder.nativeElement, geocoder.onAdd(this.map));
-
-    this.map.on('click', (event)=>{
-      const coordinates = [event.lngLat.lng, event.lngLat.lat];
-    });
-    // this.map.addControl(geocoder);
-  }
-
-  ionViewDidEnter(){
-
+    this.mapCustomService.buildMap().then(({geocoder, map})=>{
+      console.log('Tgeocoder');
+      console.log(geocoder);
+      this.renderer2.appendChild(this.asGeocoder.nativeElement, geocoder.onAdd(map));
+    })
+    .catch((err) =>{
+      console.log('ERROR: ', err)
+    })
   }
 
 }
