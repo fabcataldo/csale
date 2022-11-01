d7954409316dd089522be93dc7570cd0257a7ac3
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-10-31 23:26:08 -0300

    New dummy data added for places data set

---
+++ src/dummy_data/places.ts
@@ -0,0 +1,17 @@
+export const places = [
+    {
+        _id: 0,
+        tickets: [],
+        name: 'Johnny B. Good',
+        address: 'Av. Hipólito Yrigoyen 320',
+        number_of_people_enabled: 320,
+        lat: -64.187216,
+        lng: -31.423941,
+        social_media_urls: ['https://www.instagram.com/jbgcordoba/?hl=es'],
+        available_payment_methods: [1, 2],
+        customer_service_schedules: [1, 2, 3,4],
+        images: []
+    }
+
+
+]
