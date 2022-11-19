f492b4b4750c8ec197fc7f5f355d363007d9cf31
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-11-18 23:19:33 -0300

    New dummy data added

---
+++ src/dummy_data/customer_service_schedules.ts
@@ -0,0 +1,26 @@
+export const customer_service_schedules = [
+    {
+        _id: 0,
+        place: 0,
+        day: 4,
+        is_open: true,
+        initial_hour: '23:00',
+        final_hour: '06:00'
+    },
+    {
+        _id: 1,
+        place: 0,
+        day: 5,
+        is_open: true,
+        initial_hour: '23:00',
+        final_hour: '06:00'
+    },
+    {
+        _id: 2,
+        place: 0,
+        day: 6,
+        is_open: true,
+        initial_hour: '23:00',
+        final_hour: '06:00'
+    }
+]
