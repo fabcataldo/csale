4ed1cf6e8f2c7093277644104acee5a7ef2d2567
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-11-24 21:49:10 -0300

    Old file removed

--- src/app/services/map-state/map-state.service.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef, Injectable } from '@angular/core';
-@Injectable({
-  providedIn: 'root'
-})
-//creo que no va a servir, fijarse que onda
-export class MapStateService {
-  currentSearch: string;
-  currentSearchRef: ElementRef;
-}
