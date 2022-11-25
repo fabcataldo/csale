29a1a9e396cd01139f4a65df3d6cd75db7316eea
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-11-24 21:49:57 -0300

    CSaleTabsBar component added to csalecommoncomponents module

--- src/app/common/components/csale-common-components.module.ts
+++ src/app/common/components/csale-common-components.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { CSaleSearchbarComponent } from './csale-searchbar/csale-searchbar.component';
-
+import { CSaleTabsBarComponent } from './csale-tabs-bar/csale-tabs-bar.component';
+import { InlineSVGModule } from 'ng-inline-svg-2';
 
 @NgModule({
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    InlineSVGModule.forRoot()
   ],
-  declarations: [CSaleSearchbarComponent],
-  exports: [CSaleSearchbarComponent]
+  declarations: [CSaleSearchbarComponent, CSaleTabsBarComponent],
+  exports: [CSaleSearchbarComponent, CSaleTabsBarComponent]
 })
 export class CSaleCommonComponentsModule {}
