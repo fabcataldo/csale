8dfb5efb71182d2f5dbec25813364fc59fe36368
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-11-24 21:55:13 -0300

    Code cleaned. Old component CSaleTopTabsComponent removed. All of this in csalespecialtabs module

--- src/app/pages/special-tabs/components/csale-special-tabs-components.module.ts
+++ src/app/pages/special-tabs/components/csale-special-tabs-components.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { CSaleTopTabsComponent } from './csale-top-tabs/csale-top-tabs.component';
 import { CSaleGridCardsComponent } from './csale-grid-cards/csale-grid-cards.component';
 import { CSalePairFixedButtonsComponent } from './csale-pair-fixed-buttons/csale-pair-fixed-buttons.component';
-
 
 @NgModule({
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
   ],
-  declarations: [CSaleTopTabsComponent, CSaleGridCardsComponent, CSalePairFixedButtonsComponent],
-  exports: [CSaleTopTabsComponent, CSaleGridCardsComponent, CSalePairFixedButtonsComponent]
+  declarations: [CSaleGridCardsComponent, CSalePairFixedButtonsComponent],
+  exports: [CSaleGridCardsComponent, CSalePairFixedButtonsComponent]
 })
-export class CSaleSpecialTabsComponentsModule {}
+export class CSaleSpecialTabsComponentsModule { }
