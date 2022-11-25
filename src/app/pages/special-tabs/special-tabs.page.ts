73b00d0fb8006df014854d9b595180b601cbb4b7
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-11-24 21:53:38 -0300

    Code cleaned. Global style added. Tabs parameter added. All of this for specialtabs page

--- src/app/pages/special-tabs/special-tabs.page.ts
+++ src/app/pages/special-tabs/special-tabs.page.ts
@@ -1,18 +1,16 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { MapStateService } from 'src/app/services/map-state/map-state.service';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { BottomComponentsStateService } from 'src/app/services/bottom-components-state/bottom-components-state.service';
 
 @Component({
   selector: 'app-special-tabs',
   templateUrl: './special-tabs.page.html',
   styleUrls: ['./special-tabs.page.scss'],
 })
-export class SpecialTabsPage implements OnInit {
+export class SpecialTabsPage {
   @ViewChild('search') public searchElementRef: ElementRef;
-  constructor(private mapStateService: MapStateService) { }
 
-  ngOnInit() {
-  }
+  constructor( public bottomCompStateService: BottomComponentsStateService) { }
+
 
   onSearchInputChange(event){
     console.log('event')
