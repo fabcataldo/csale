98461c51b50c9755e43a08af84f37a13a9cf21f8
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-11-24 21:48:02 -0300

    Bottom action bar added in app level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BottomComponentsStateService } from './services/bottom-components-state/bottom-components-state.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor( public bottomCompStateService: BottomComponentsStateService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CSaleCommonComponentsModule } from './common/components/csale-common-components.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    CSaleCommonComponentsModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
