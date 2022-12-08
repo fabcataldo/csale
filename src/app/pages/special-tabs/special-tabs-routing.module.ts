5944751826d16527dbaf2126b842b744efa115fa
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-12-07 23:32:27 -0300

    Routes added in special-tabs routing module,

--- src/app/pages/special-tabs/special-tabs-routing.module.ts
+++ src/app/pages/special-tabs/special-tabs-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { SpecialTabsPage } from './special-tabs.page';
-import { TabEventsPage } from './tab-events/tab-events.page';
-import { TabPlacesPage } from './tab-places/tab-places.page';
-import { TabEventsForYouPage } from './tab-events-for-you/tab-events-for-you.page';
 
 const routes: Routes = [
   {
@@ -13,22 +9,27 @@ const routes: Routes = [
     children: [
       {
         path: 'events-for-you',
-        loadChildren: () => import('./tab-events-for-you/tab-events-for-you.module').then( m => m.TabTypeEventsForYouPageModule)
+        loadChildren: () => import('./tab-events-for-you/tab-events-for-you.module').then(m => m.TabTypeEventsForYouPageModule)
       },
       {
         path: 'places',
-        loadChildren: () => import('./tab-places/tab-places.module').then( m => m.TabPlacesPageModule)
+        loadChildren: () => import('./tab-places/tab-places.module').then(m => m.TabPlacesPageModule)
       },
       {
         path: 'events',
-        loadChildren: () => import('./tab-events/tab-events.module').then( m => m.TabEventsPageModule)
+        loadChildren: () => import('./tab-events/tab-events.module').then(m => m.TabEventsPageModule)
       },
-    ]
+    ],
+  },
+  {
+    path: 'grid-cards-page/:tag',
+    loadChildren: () => import('./grid-cards-page/grid-cards-page.module').then(m => m.GridCardsPagePageModule)
   }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class SpecialTabsPageRoutingModule {}
+export class SpecialTabsPageRoutingModule { }
