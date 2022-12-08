a4be0a25e969f43a00473622072132bb92f59821
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-12-07 23:31:51 -0300

    CSaleCommonComponentsModule added in place map module

--- src/app/pages/places-map/places-map.module.ts
+++ src/app/pages/places-map/places-map.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { PlacesMapPageRoutingModule } from './places-map-routing.module';
 
 import { PlacesMapPage } from './places-map.page';
-import { CSaleCommonComponentsModule } from 'src/app/common/components/csale-common-components.module';
+import { CSaleCommonComponentsModule } from 'src/app/common/csale-common-components.module';
 import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
