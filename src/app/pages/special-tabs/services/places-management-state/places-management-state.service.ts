9064bd8017608b75498e9b1c074b2ede8f31b73a
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-12-07 23:22:30 -0300

    New placemanagementstate service added

---
+++ src/app/pages/special-tabs/services/places-management-state/places-management-state.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { CurrentPlace } from 'src/app/models/currentPlaces';
+import { Place } from 'src/app/models/place';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesManagementStateService {
+  //aca se va a manejar la lista de lugares para las pags del special-tabs
+  //para mandarlas al mapa, cuando se detecte el click del boton "Mapa"
+  //cuando el usuario esté en una de las pags de las cards del inicio de special-tabs
+  currentPlaces: Array<CurrentPlace> = [];
+}
